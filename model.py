52296e270f0280a936c055e3b4a66206441da6b4
Harsh Ranjan <139263707+Harsh281109@users.noreply.github.com>
2026-04-29 07:39:03 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,15 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 
+# Load dataset
+data = pd.read_csv("data.csv")
+
+X = data[['energy_usage', 'waste', 'water_usage']]
+y = data['carbon_emission']
+
+# Train model
+model = RandomForestRegressor()
+model.fit(X, y)
+
+def predict(energy, waste, water):
+    return model.predict([[energy, waste, water]])[0]
